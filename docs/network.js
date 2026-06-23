5ebf69d60f7353bde83efe9c54e6bbbe34501d47
h-connor <62314329+h-connor@users.noreply.github.com>
2026-06-23 13:42:04 -0500

    url test

--- docs/network.js
+++ docs/network.js
@@ -612,6 +612,7 @@ var patterns_all = null;
 var client = new XMLHttpRequest();
 var tot_loaded_files = 0
 
+console.log(new URL("data/file.json", window.location.href));
 client.open('GET', '/data/_contrasted_final_results_CONTRAST_PREFIX.txt', true);
 client.onreadystatechange = function () {
   if (client.readyState === 4 && client.status === 200) {
